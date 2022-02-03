02e48f731b537e013902f09344ad013f53a7b0c1
Oliver <mrtronn@users.noreply.github.com>
2022-02-03 22:09:18 +0700

    tweak

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 
 setup(
     name="pyradapi",
-    version="0.14",
+    version="0.141",
     packages=["pyradapi"],
     description="A python package to use radix api easily",
     license="Invariance Pte Limited",
