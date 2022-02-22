4f52926727837bcfe8fac64035714d28cdd25a6f
Oliver <mrtronn@users.noreply.github.com>
2022-02-22 16:59:16 +0700

    tweek

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import setuptools
 
 setuptools.setup(
     name="pyradapi",
-    version="0.151",
+    version="0.152",
     packages=["pyradapi"],
     description="A python package to use radix api easily",
     license="Invariance Pte Limited",
