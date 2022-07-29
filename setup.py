db96d78fc9a823aa20e8ae7ba7e8bda5aa7530a2
Oliver <mrtronn@users.noreply.github.com>
2022-07-29 21:16:43 +0800

    version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import setuptools
 
 setuptools.setup(
     name="pyradapi",
-    version="0.155",
+    version="0.16",
     # packages=["pyradapi"],
     description="A python package to use radix api easily",
     license="Invariance Pte Limited",
